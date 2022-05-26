6be332158f99d1a75c536e4e5e6bedfd4c4d5402
NxKarim <43686802+NxKarim@users.noreply.github.com>
2022-05-26 15:26:52 -0500

    Added Command Handler and the listemojis command.
    Command handler is smol, may be replaced with slash commands later.
    Getemojis function could be refined more.

--- src/listeners/messageReader.js
+++ src/listeners/messageReader.js
@@ -1,11 +1,29 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+const { MessageActionRow, MessageButton } = require('discord.js');
 exports.default = (client) => {
     client.on("messageCreate", async (message) => {
         //return if the message was sent by a bot, or if the message doesn't contain any emotes or if it's in a DM (no webhooks in dms)
         //yes there's a lot of exclamation marks, they're all guarded but typescript is a bit dumb sometimes.
-        if (message.author.bot || message.content.match(/(?!<a?):[^<:>]+?:(?!\d+>)/g) === null || message.channel.type === "DM") {
+        if (message.author.bot || message.channel.type === "DM") {
             return;
+        }
+        //A very simple command handler. 
+        if (message.content.startsWith(process.env.PREFIX)) {
+            const args = message.content.slice(process.env.PREFIX.length).split(/ +/);
+            const entercommand = args.shift().toLowerCase();
+            switch (true) {
+                case (entercommand == `listemojis`): {
+                    getEmojis(message, client);
+                }
+                default: {
+                    return;
+                }
+            }
+        }
+        //Check for emote matches moved, so commands can be used.
+        if (message.content.match(/(?!<a?):[^<:>]+?:(?!\d+>)/g) === null){
+             return; 
         }
         let fixedMessage = await fixPoorMessage(message, client);
         if (fixedMessage === undefined) {
@@ -54,13 +72,18 @@ const fetchWebhook = async (message) => {
     }
 };
 const fixPoorMessage = async (message, client) => {
-    //let match = message.content.match(/:(.+?):/g)
+    let match = message.content.match(/:(.+?):/g);
     let emojis = client.emojis.cache;
+    let guildEmojis = message.guild.emojis.cache;
     //cache isn't infallable, need to refetch failed emotes
+    let filteredEmojis = emojis.filter(emoji => match.includes(`:${emoji.name}:`));
+    if (filteredEmojis.filter(emoji => emoji.animated).size <= 0) {
+        filteredEmojis.sweep((emoji, key) => guildEmojis.has(key));
+    }
     let output;
-    if (emojis.size > 0) {
+    if (filteredEmojis.size > 0) {
         output = message.content;
-        for (const emoji of emojis.values()) {
+        for (const emoji of filteredEmojis.values()) {
             output = output.replace(new RegExp(`:${emoji.name}:`, "g"), `<${emoji.animated ? "a" : ""}:${emoji.name}:${emoji.id}>`);
         }
     }
@@ -79,4 +102,105 @@ const sendFixedMessage = (message, author, webhook, threadId) => {
         .catch(e => {
         console.error(e);
     });
+};
+
+//Command function used to get all the emojis available for the bot. 
+const getEmojis = async (message, client) => {
+    let guilds = client.guilds.cache;
+    guilds = [...guilds.keys()];
+   //Buttons yay!
+    var rowb = new MessageActionRow()
+        .addComponents(
+            new MessageButton()
+                .setEmoji('⬅️')
+                .setCustomId('back')
+                .setLabel('')
+                .setStyle('SUCCESS'),
+
+            new MessageButton()
+                .setEmoji('➡️')
+                .setCustomId('fow')
+                .setLabel('')
+                .setStyle('SUCCESS'),
+
+            new MessageButton()
+                .setEmoji('❌')
+                .setCustomId('cancel')
+                .setLabel('Close')
+                .setStyle('DANGER'),
+        );
+
+    const emoteEmbed = {
+        color: 0x8F00FF,
+        title: 'Not Quite Tako',
+        description: 'Here are the lists of all the emotes that NQT has access to. Use the arrows to move between available servers.',
+        thumbnail: {
+            url: `https://cdn.discordapp.com/attachments/887967589031116820/979246559059382282/TakoRock.png`,
+        },
+        fields: [],
+        footer: {
+            text: 'Made by just_a_pyro#9060 and a little help of NxKarim#1744.',
+        },
+    };
+
+    var sentmessage = await message.channel.send({ embeds: [emoteEmbed], components: [rowb] });
+    const collector = sentmessage.createMessageComponentCollector({ componentType: 'BUTTON', time: 120000 });
+
+    var co = 0,listnum = guilds.length-1;
+    collector.on('collect',async i => {
+        if (i.user.id === message.author.id) {
+            switch (i.customId) {
+                case `fow`: {
+                    co += 1;
+                    if (co > listnum) { co = 0 };
+                    emoteEmbed.fields = [];
+                    let emojis, output = ``, lists = [];
+                    var eguild = client.guilds.cache.get(`${guilds[co]}`);
+                    emoteEmbed.description = `Here is a list of all the emotes that NQT has access to.\n**【${eguild.name}】**`
+                    emojis = eguild.emojis.cache;
+                    for (const emoji of emojis.values()) {
+                        output += `<${emoji.animated ? "a" : ""}:${emoji.name}:${emoji.id}> \`\`:${emoji.name}:\`\`\n`;
+                    }
+                    lists = output.match(/[\s\S]{1,1024}(?=\n)/g);
+                    for (const display of lists) {
+                        emoteEmbed.fields.push({ name: `⠀`, value: `${display}`, inline: true })
+                    }
+
+                    emoteEmbed.footer.text = `Made by just_a_pyro#9060 and a little help of NxKarim#1744. Server: ${co + 1}/${listnum + 1}`;
+                    sentmessage.edit({ embeds: [emoteEmbed], components: [rowb] }).then(t => i.deferUpdate())
+
+                    break;
+                }
+                case `back`: {
+                    co -= 1;
+                    if (co < 0) { co = listnum }
+                    emoteEmbed.fields = [];
+                    let emojis, output = ``, lists = [];
+                    var eguild = client.guilds.cache.get(`${guilds[co]}`);
+                    emoteEmbed.description = `Here is a list of all the emotes that NQT has access to.\n**【${eguild.name}】**`
+                    emojis = eguild.emojis.cache;
+                    for (const emoji of emojis.values()) {
+                        output += `<${emoji.animated ? "a" : ""}:${emoji.name}:${emoji.id}> \`\`:${emoji.name}:\`\`\n`;
+                    }
+                    lists = output.match(/[\s\S]{1,1024}(?=\n)/g);
+                    for (const display of lists) {
+                        emoteEmbed.fields.push({ name: `⠀`, value: `${display}`, inline: true })
+                    }
+
+                    emoteEmbed.footer.text = `Made by just_a_pyro#9060 and a little help of NxKarim#1744. Server: ${co + 1}/${listnum + 1}`;
+                    sentmessage.edit({ embeds: [emoteEmbed], components: [rowb] }).then(t => i.deferUpdate())
+
+                    break;
+                }
+                case `cancel`: {
+                    sentmessage.edit({ embeds: [emoteEmbed], components: [] })
+                    break;
+                }
+            }
+
+        } else {
+            return;
+        }
+    });
+
 };
