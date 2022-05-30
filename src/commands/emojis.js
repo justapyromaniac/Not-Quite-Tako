541b4272356893f03226c1b035a07094cc7af7d0
NxKarim <43686802+NxKarim@users.noreply.github.com>
2022-05-29 21:55:34 -0500

    Now displays all emotes from each server.
    Big change!, now it works almost as intented.

--- src/commands/emojis.js
+++ src/commands/emojis.js
@@ -1,17 +1,18 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const { MessageActionRow, MessageButton } = require('discord.js');
 module.exports = {
-	data: new SlashCommandBuilder()
-		.setName('emojis')
-		.setDescription('Shows all available emojis.'),
-	async execute(interaction) {
+    data: new SlashCommandBuilder()
+        .setName('emojis')
+        .setDescription('Shows all available emojis.'),
+    async execute(interaction) {
         const client = interaction.client;
 
-        const getEmojis = async (interaction,client) => {
+        const getEmojis = async (interaction, client) => {
+            console.log(`Tako emojis`);
             let guilds = client.guilds.cache;
-            
+
             guilds = [...guilds.keys()];
-        
+
             var rowb = new MessageActionRow()
                 .addComponents(
                     new MessageButton()
@@ -19,20 +20,19 @@ module.exports = {
                         .setCustomId('back')
                         .setLabel('')
                         .setStyle('SUCCESS'),
-        
+
                     new MessageButton()
                         .setEmoji('➡️')
                         .setCustomId('fow')
                         .setLabel('')
                         .setStyle('SUCCESS'),
-        
                     new MessageButton()
                         .setEmoji('❌')
                         .setCustomId('cancel')
                         .setLabel('Close')
                         .setStyle('DANGER'),
                 );
-        
+
             const emoteEmbed = {
                 color: 0x8F00FF,
                 title: 'Not Quite Tako',
@@ -45,85 +45,87 @@ module.exports = {
                     text: 'Made by just_a_pyro#9060 and a little help of NxKarim#1744.',
                 },
             };
-        
-            await interaction.reply({ embeds: [emoteEmbed], components: [rowb], fetchReply: true, ephemeral: true});
-            const collector = interaction.channel.createMessageComponentCollector({ componentType: 'BUTTON', time: 120000});
-            var co = 0, listnum = guilds.length - 1;
-        
+
+            await interaction.reply({ embeds: [emoteEmbed], components: [rowb], fetchReply: true, ephemeral: true });
+            const collector = interaction.channel.createMessageComponentCollector({ componentType: 'BUTTON', time: 200000 });
+            let co = 0, guildoutputs = [], guildnames = [];
+
+            let allguilds = client.guilds.cache.values();
+
+            for (const guild of allguilds) {
+                let emojis, output = ``;
+                emojis = guild.emojis.cache;
+                if (guild.id == interaction.guildId) { emojis = emojis.filter(em => em.animated == true); }
+                for (const emoji of emojis.values()) { output += `<${emoji.animated ? "a" : ""}:${emoji.name}:${emoji.id}> \`\`:${emoji.name}:\`\`\n`; }
+                output = output.match(/[\s\S]{1,5000}(?=\n)/g);
+                for(let pages of output){
+                    guildnames.push(guild.name);
+                    guildoutputs.push(pages.match(/[\s\S]{1,1024}(?=\n)/g));
+                }
+            }
+
+            let listnum = guildoutputs.length - 1;
+
             collector.on('collect', async i => {
                 if (i.user.id === interaction.user.id) {
                     switch (i.customId) {
                         case `fow`: {
                             co += 1;
                             if (co > listnum) { co = 0 };
-                           
+
+                            let emojilists = guildoutputs[co];
+                            let guildname = guildnames[co];
+
                             emoteEmbed.fields = [];
-                            let emojis, output = ``, lists = [];
-                            var eguild = client.guilds.cache.get(`${guilds[co]}`);
-                            emoteEmbed.description = `Here is a list of all the emotes that NQT has access to.\n**【${eguild.name}】**`
-                            emoteEmbed.footer.text = `Made by just_a_pyro#9060 and a little help of NxKarim#1744. Server: ${co+1}/${listnum+1}`;
-                            emojis = eguild.emojis.cache;
-                            if(eguild.id == interaction.user.id){
-                                emojis = emojis.filter(em => em.animated == true);
+                            emoteEmbed.description = `Here is a list of all the emotes that NQT has access to.\n**【${guildname}】**`
+                            emoteEmbed.footer.text = `Made by just_a_pyro#9060 and a little help of NxKarim#1744. Page: ${co + 1}/${listnum + 1}`;
+
+                            if (emojilists != null) {
+                                for (const fieldvalues of emojilists) {
+                                    emoteEmbed.fields.push({ name: `⠀`, value: `${fieldvalues}`, inline: true });
+                                }
                             }
-                            for (const emoji of emojis.values()) {
-                                output += `<${emoji.animated ? "a" : ""}:${emoji.name}:${emoji.id}> \`\`:${emoji.name}:\`\`\n`;
-                            }
-                        
-                            lists = output.match(/[\s\S]{1,1024}(?=\n)/g);
-                            if(lists){
-                            for (const display of lists) {
-                                emoteEmbed.fields.push({ name: `⠀`, value: `${display}`, inline: true })
-                                if(emoteEmbed.fields.length >=5){break;}
-                            }}
-                        
-                           await interaction.editReply({ embeds: [emoteEmbed], components: [rowb], ephemeral: true}).then(t => i.deferUpdate())
-        
+
+                            await interaction.editReply({ embeds: [emoteEmbed], components: [rowb], ephemeral: true }).then(t => i.deferUpdate())
+
                             break;
                         }
                         case `back`: {
                             co -= 1;
                             if (co < 0) { co = listnum }
-                            
+
+                            let emojilists = guildoutputs[co];
+                            let guildname = guildnames[co];
+
                             emoteEmbed.fields = [];
-                            let emojis, output = ``, lists = [];
-                            var eguild = client.guilds.cache.get(`${guilds[co]}`);
-                            emoteEmbed.description = `Here is a list of all the emotes that NQT has access to.\n**【${eguild.name}】**`
-                            emoteEmbed.footer.text = `Made by just_a_pyro#9060 and a little help of NxKarim#1744. Server: ${co+1}/${listnum+1}`;
-                            emojis = eguild.emojis.cache;
-                            if(eguild.id == interaction.user.id){
-                                emojis = emojis.filter(em => em.animated == true);
+                            emoteEmbed.description = `Here is a list of all the emotes that NQT has access to.\n**【${guildname}】**`
+                            emoteEmbed.footer.text = `Made by just_a_pyro#9060 and a little help of NxKarim#1744. Page: ${co + 1}/${listnum + 1}`;
+
+                            if (emojilists != null) {
+                                for (const fieldvalues of emojilists) {
+                                    emoteEmbed.fields.push({ name: `⠀`, value: `${fieldvalues}`, inline: true });
+                                }
                             }
-                            for (const emoji of emojis.values()) {
-                                output += `<${emoji.animated ? "a" : ""}:${emoji.name}:${emoji.id}> \`\`:${emoji.name}:\`\`\n`;
-                            }
-                        
-                            lists = output.match(/[\s\S]{1,1024}(?=\n)/g);
-                            if(lists){
-                            for (const display of lists) {
-                                emoteEmbed.fields.push({ name: `⠀`, value: `${display}`, inline: true })
-                                if(emoteEmbed.fields.length >=5){break;}
-                            }}
-                        
-                           await interaction.editReply({ embeds: [emoteEmbed], components: [rowb], ephemeral: true }).then(t => i.deferUpdate())
-                            
+
+                            await interaction.editReply({ embeds: [emoteEmbed], components: [rowb], ephemeral: true }).then(t => i.deferUpdate())
+
                             break;
                         }
                         case `cancel`: {
-                           await interaction.editReply({ embeds: [emoteEmbed], components: [] })
+                            await interaction.editReply({ embeds: [emoteEmbed], components: [] })
                             break;
                         }
                     }
-        
+
                 } else {
                     return;
                 }
             });
-        
+
         };
 
-       await getEmojis(interaction, client);
+        await getEmojis(interaction, client);
 
-      
-	},
+
+    },
 };
